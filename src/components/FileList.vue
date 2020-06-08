71f08dc7f3849c151f2d6f1c280778c31580ae4c
yangchen <amerkiousy@outlook.com>
2020-06-08 13:59:05 +0800

    add file list

---
+++ src/components/FileList.vue
@@ -0,0 +1,42 @@
+<template>
+  <el-scrollbar class="file-list" wrap-class="scrollbar-filelist" :noresize="false" tag="ul">
+    <li v-for="(item, index) in fileList" :key="index" class="file-item">
+      <font-awesome-icon :icon="['fab', 'markdown']" class="item-icon" />
+      <p class="item-title">{{ item.title }}</p>
+      <p class="item-time">{{ item.time }}</p>
+    </li>
+  </el-scrollbar>
+</template>
+
+<script>
+export default {
+  name: "FileList",
+  props: {
+    fileList: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.scrollbar-filelist {
+  height: calc(100vh - 56px);
+  overflow-x: hidden !important;
+}
+.el-scrollbar__bar {
+  opacity: 1;
+
+  &.is-vertical {
+    right: 0px;
+    width: 5px;
+    .el-scrollbar__thumb {
+      background-color: rgba(144, 147, 153, 0.5);
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,54 @@
 <template>
   <div class="app-warpper">
-   <div class="siderbar-container">
-     <file-search ></file-search>
-   </div>
-   <div class="main-container"></div>
+    <div class="siderbar-container">
+      <file-search v-model="searchTitle"></file-search>
+      <file-list :fileList="fileList"></file-list>
+    </div>
+    <div class="main-container"></div>
   </div>
 </template>
 
 <script>
-import FileSearch from '@/components/FileSearch';
+import FileSearch from "@/components/FileSearch";
+import FileList from "@/components/FileList";
+
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    FileSearch
+    FileSearch,
+    FileList
   },
   data() {
     return {
-       searchTitle: '',
-    }
-  },
-}
+      searchTitle: "",
+      fileList: [
+        { id: 1, title: "文件名 1", time: "2020-06-21" },
+        { id: 2, title: "文件名 2", time: "2020-06-21" },
+        { id: 3, title: "文件名 3", time: "2020-06-21" },
+        { id: 4, title: "文件名 4", time: "2020-06-21" },
+        { id: 5, title: "文件名 5", time: "2020-06-21" },
+        { id: 6, title: "文件名 6", time: "2020-06-21" },
+        { id: 1, title: "文件名 1", time: "2020-06-21" },
+        { id: 2, title: "文件名 2", time: "2020-06-21" },
+        { id: 3, title: "文件名 3", time: "2020-06-21" },
+        { id: 4, title: "文件名 4", time: "2020-06-21" },
+        { id: 5, title: "文件名 5", time: "2020-06-21" },
+        { id: 6, title: "文件名 6", time: "2020-06-21" }
+      ]
+    };
+  }
+};
 </script>
 
 <style lang="less" scoped>
-.app-warpper{
+.app-warpper {
   display: flex;
-  .siderbar-container{
+  .siderbar-container {
     width: 300px;
     height: 100vh;
-    border-right:1px solid #eaeefb
+    border-right: 1px solid #eaeefb;
   }
-  .main-container{
+  .main-container {
     flex: 1;
   }
 }
